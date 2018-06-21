b802fb9f5725b751967a6adc3d2dbe18b9594b30
William Assis <williamf1f2@gmail.com>
2018-06-21 12:06:37 -0300

    Adicionar POST na API

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
+const bodyParser = require('body-parsen');
 const app = express();
+app.use(bodyParser.json());
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello, world');
@@ -12,5 +15,14 @@ app.get('/pudim', (req, res) => {
         cobertura: "caramelo",
         adicional: "brigadeiro"
     });
+});
+
+app.get('/ola', (req, res) => {
+     res.send('olaaaaaaaaa');
+});
+
+app.post('/teste', (req, res) => {
+    res.send('recebi um post');
+});
 
 app.listen(3000); //em desenvolvimento, usar porta 3000, o processo roda nesa porta de acesso
